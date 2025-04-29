4b7699576498ab047011c0d1bc67290cde71cc53
Artur Kovalchuk <johndoe@example.com>
2025-04-29 20:04:58 +0300

    basemodel

--- backend_api/app/applicatoins/users/router.py
+++ backend_api/app/applicatoins/users/router.py
@@ -1,8 +1,11 @@
-from fastapi import APIRouter , status
+from fastapi import APIRouter, status
+from applicatoins.users.schemas import RegisterUserFields, BaseFields, BaseFields
 
 router_users = APIRouter()
 
 
-@router_users.post('/')
-async def create_user():
-    return{"st" : 200}
+@router_users.post("/create", status_code=status.HTTP_201_CREATED)
+async def create_user(
+    new_user: RegisterUserFields
+) -> BaseFields:
+    return new_user
