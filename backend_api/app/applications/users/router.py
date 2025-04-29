c00ddd2b969fe87dc883c1c664ff63ce831a2a69
Vasyl <vasyl@example.com>
2025-04-29 19:38:47 +0300

    userfields

--- backend_api/app/applications/users/router.py
+++ backend_api/app/applications/users/router.py
@@ -1,11 +1,11 @@
 from fastapi import APIRouter, status
-from applications.users.schemas import UserFields
+from applications.users.schemas import RegisterUserFields, BaseFields
 
 router_users = APIRouter()
 
 
 @router_users.post("/create", status_code=status.HTTP_201_CREATED)
 async def create_user(
-    new_user: UserFields
-) -> UserFields:
+    new_user: RegisterUserFields
+) -> BaseFields:
     return new_user
--- backend_api/app/applications/users/schemas.py
+++ backend_api/app/applications/users/schemas.py
@@ -1,7 +1,13 @@
 from pydantic import BaseModel, EmailStr, Field
 
 
-class UserFields(BaseModel):
+class BaseFields(BaseModel):
     email: EmailStr = Field(description='User email', examples=['test_hillel_api_mailing@ukr.net'])
     name: str = Field(description='User nickname', examples=['Casper'])
+
+class PasswordField(BaseModel):
     password: str = Field(min_length=8)
+
+
+class RegisterUserFields(BaseFields, PasswordField):
+    pass
