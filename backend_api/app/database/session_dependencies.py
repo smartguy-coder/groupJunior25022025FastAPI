35215160432244caf6c79526ab26626e66043014
Vasyl <vasyl@example.com>
2025-05-09 19:54:17 +0300

    session

--- backend_api/app/database/session_dependencies.py
+++ backend_api/app/database/session_dependencies.py
@@ -1,8 +1,13 @@
-from sqlalchemy.ext.asyncio import  create_async_engine
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 
 from settings import settings
 
-
 engine = create_async_engine(settings.DATABASE_URL_ASYNC, echo=True)
+async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
 
-
+async def get_async_session():
+    async with async_session_maker() as session:
+        try:
+            yield session
+        finally:
+            await session.close()
