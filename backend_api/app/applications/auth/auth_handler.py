5c17ef9c84982b987498d7e10ec811c7ff08559c
Vasyl <vasyl@example.com>
2025-05-16 19:52:47 +0300

    set jwt secret

--- backend_api/app/applications/auth/auth_handler.py
+++ backend_api/app/applications/auth/auth_handler.py
@@ -1,5 +1,20 @@
+from settings import settings
+from fastapi import APIRouter, Depends
+from fastapi.security import OAuth2PasswordRequestForm
+
+from sqlalchemy.ext.asyncio import AsyncSession
+
+
+from database.session_dependencies import get_async_session
 
 
 class AuthHandler:
     def __init__(self):
+        self.secret = settings.JWT_SECRET
+        self.algorithm = settings.JWT_ALGORITHM
+
+    async def get_login_token_pairs(self, data: OAuth2PasswordRequestForm, session: AsyncSession):
         pass
+
+
+auth_handler = AuthHandler()
--- backend_api/app/applications/auth/router.py
+++ backend_api/app/applications/auth/router.py
@@ -3,7 +3,7 @@ from fastapi.security import OAuth2PasswordRequestForm
 
 from sqlalchemy.ext.asyncio import AsyncSession
 
-
+from applications.auth.auth_handler import auth_handler
 from database.session_dependencies import get_async_session
 
 
@@ -15,6 +15,6 @@ async def user_login(
     data: OAuth2PasswordRequestForm = Depends(),
     session: AsyncSession = Depends(get_async_session),
 ):
-
-    return
+    token_pair = await auth_handler.get_login_token_pairs(data, session)
+    return token_pair
 
