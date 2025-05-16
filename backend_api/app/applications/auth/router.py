4c74cdce91082de666a39a166058111a77b16f05
Vasyl <vasyl@example.com>
2025-05-16 19:36:31 +0300

    provide form and session

--- backend_api/app/applications/auth/router.py
+++ backend_api/app/applications/auth/router.py
@@ -1,10 +1,20 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from fastapi.security import OAuth2PasswordRequestForm
+
+from sqlalchemy.ext.asyncio import AsyncSession
+
+
+from database.session_dependencies import get_async_session
 
 
 router_auth = APIRouter()
 
 
 @router_auth.post('/login')
-async  def user_login():
+async def user_login(
+    data: OAuth2PasswordRequestForm = Depends(),
+    session: AsyncSession = Depends(get_async_session),
+):
+
     return
 
