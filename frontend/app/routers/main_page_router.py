50c26cfee4fa67b539f75f17d59a8c0d0be0eaad
Vasyl <vasyl@example.com>
2025-05-27 19:35:37 +0300

    templates

--- frontend/app/routers/main_page_router.py
+++ frontend/app/routers/main_page_router.py
@@ -1,6 +1,10 @@
 from fastapi import APIRouter
+from fastapi.templating import Jinja2Templates
 
 router = APIRouter()
+
+templates = Jinja2Templates(directory='templates')
+
 
 @router.get('/')
 async def index():
