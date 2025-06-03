598953c4edd6f4195cc81111eb324f5b084128a4
Vasyl <vasyl@example.com>
2025-06-03 20:29:00 +0300

    login user

--- frontend/app/routers/main_page_router.py
+++ frontend/app/routers/main_page_router.py
@@ -9,7 +9,6 @@ router = APIRouter()
 templates = Jinja2Templates(directory='templates')
 
 
-
 @router.get('/')
 async def index(request: Request, user: dict = Depends(get_current_user_with_token)):
     context = {'request': request}
@@ -19,14 +18,11 @@ async def index(request: Request, user: dict = Depends(get_current_user_with_tok
     return response
 
 
-
-
 @router.get('/login')
 @router.post('/login')
 async def login(request: Request, user: dict = Depends(get_current_user_with_token), user_email: str = Form(''),
                 password: str = Form('')):
     context = {'request': request, "entered_email": user_email}
-    print(user, 88888888888888888888888888)
     redirect_url = request.url_for("index")
     if user.get('name'):
         response = RedirectResponse(redirect_url, status_code=status.HTTP_303_SEE_OTHER)
@@ -43,8 +39,6 @@ async def login(request: Request, user: dict = Depends(get_current_user_with_tok
         errors = ["Incorrect login or password"]
         context['errors'] = errors
         return templates.TemplateResponse('login.html', context=context)
-    print(6666666666666666666666666)
-
     response = RedirectResponse(redirect_url, status_code=status.HTTP_303_SEE_OTHER)
     response.set_cookie(key="access_token", value=access_token, httponly=True, max_age=60 * 5)
     return response
@@ -55,4 +49,35 @@ async def logout(request: Request):
     redirect_url = request.url_for("login")
     response = RedirectResponse(redirect_url, status_code=status.HTTP_303_SEE_OTHER)
     response.delete_cookie('access_token')
+    return response
+
+
+@router.get('/register')
+@router.post('/register')
+async def register(
+        request: Request,
+        user: dict = Depends(get_current_user_with_token),
+        user_email: str = Form(''),
+        password: str = Form(''),
+        name: str = Form(''),
+):
+    context = {'request': request, "entered_email": user_email}
+    redirect_url = request.url_for("index")
+    if user.get('name'):
+        response = RedirectResponse(redirect_url, status_code=status.HTTP_303_SEE_OTHER)
+        return response
+
+    if request.method == "GET":
+        response = templates.TemplateResponse('login.html', context=context)
+        response.delete_cookie('access_token')
+        return response
+
+    user_tokens = await login_user(user_email, password)
+    access_token = user_tokens.get('access_token')
+    if not access_token:
+        errors = ["Incorrect login or password"]
+        context['errors'] = errors
+        return templates.TemplateResponse('login.html', context=context)
+    response = RedirectResponse(redirect_url, status_code=status.HTTP_303_SEE_OTHER)
+    response.set_cookie(key="access_token", value=access_token, httponly=True, max_age=60 * 5)
     return response
