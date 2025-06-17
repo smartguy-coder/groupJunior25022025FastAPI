cd0ec78e1c1c5b7f2ce3457c0162a33236832c03
Vasyl <vasyl@example.com>
2025-06-17 20:30:04 +0300

    products

--- backend_api/app/applications/products/router.py
+++ backend_api/app/applications/products/router.py
@@ -1,10 +1,16 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Body, UploadFile
 
 products_router = APIRouter()
 
 
 @products_router.post('/')
-async def create_product():
+async def create_product(
+    main_image: UploadFile,
+    images: list[UploadFile],
+    title: str = Body(max_length=100),
+    description: str = Body(max_length=1000),
+    price: float = Body(gt=1),
+):
     return
 
 
