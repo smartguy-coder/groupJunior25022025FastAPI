e9a9a64608d78ad9affa88d2191395c7cdb4df31
Vasyl <vasyl@example.com>
2025-06-17 20:47:53 +0300

    products

--- backend_api/app/services/s3/s3.py
+++ backend_api/app/services/s3/s3.py
@@ -1,25 +1,44 @@
-import boto3
+import aioboto3
+from fastapi import APIRouter, Body, UploadFile
 
 ACCESS_KEY = 'ba88124bb9f211337bbc6c103330c249'
 SECRET_KEY = '863ba92413967d939d8cd1296b9010d62099712bb76c44fde5ffcea771822180'
 BUCKET_NAME = 'group25022025'
 ENDPOINT = 'https://8721af4803f2c3c631a90d8b64d397b7.r2.cloudflarestorage.com'
-# ---------------------------------------------------------------------------
-
-
 PUBLIC_URL = 'https://pub-d2b580fe400441b19434564174b8efa7.r2.dev'
 
-s3client = boto3.client(
-    's3',
-    endpoint_url=ENDPOINT,
-    aws_access_key_id=ACCESS_KEY,
-    aws_secret_access_key=SECRET_KEY,
-    region_name='EEUR'
-)
+# s3client = boto3.client(
+#     's3',
+#     endpoint_url=ENDPOINT,
+#     aws_access_key_id=ACCESS_KEY,
+#     aws_secret_access_key=SECRET_KEY,
+#     region_name='EEUR'
+# )
 
 
 # UPLOAD FILE
 
-target_file_name = 'images/66666666666666666.jpeg'
-s3client.upload_file('spring.jpeg', BUCKET_NAME, target_file_name)
-print(f'{PUBLIC_URL}/{target_file_name}')
+# target_file_name = 'images/66666666666666666.jpeg'
+# s3client.upload_file('spring.jpeg', BUCKET_NAME, target_file_name)
+# print(f'{PUBLIC_URL}/{target_file_name}')
+
+
+class  S3Storage:
+    def __init__(self):
+        self.bucket_name = BUCKET_NAME
+
+    async def get_s3_session(self):
+        session = aioboto3.Session()
+        async with session.client(
+            's3',
+            endpoint_url=ENDPOINT,
+            aws_access_key_id=ACCESS_KEY,
+            aws_secret_access_key=SECRET_KEY,
+            region_name='EEUR'
+        ) as s3:
+            yield s3
+
+    async def upload_file(self) -> str:
+        return ''
+
+s3_storage = S3Storage()
