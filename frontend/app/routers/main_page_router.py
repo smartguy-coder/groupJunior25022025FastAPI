05028bb50d8671896c18e59e32863e2e0c7d6abb
Vasyl <vasyl@example.com>
2025-06-27 19:44:01 +0300

    extract productt front 2

--- frontend/app/routers/main_page_router.py
+++ frontend/app/routers/main_page_router.py
@@ -23,6 +23,19 @@ async def index(request: Request, query: str = Form(''), user: dict = Depends(ge
     return response
 
 
+@router.get('/product/{product_id}')
+async def product_detail(request: Request, product_id: int, user: dict = Depends(get_current_user_with_token)):
+    # products = await get_products(query)
+    context = {
+        'request': request,
+        # "product": product,
+    }
+    if user.get('name'):
+        context['user'] = user
+    response = templates.TemplateResponse('product_detail.html', context=context)
+    return response
+
+
 @router.get('/login')
 @router.post('/login')
 async def login(request: Request, user: dict = Depends(get_current_user_with_token), user_email: str = Form(''),
