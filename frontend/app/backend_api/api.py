2e675080229c0e1ddf73834d197c07062cc9d3aa
Vasyl <vasyl@example.com>
2025-06-27 20:09:02 +0300

    extract product front 3

--- frontend/app/backend_api/api.py
+++ frontend/app/backend_api/api.py
@@ -51,3 +51,11 @@ async def get_products(q: str = ""):
 
         )
         return response.json()
+
+
+async def get_product(pk: int):
+    async with httpx.AsyncClient() as client:
+        response = await client.get(
+            url=f'{settings.BACKEND_API}products/{pk}',
+        )
+        return response.json()
